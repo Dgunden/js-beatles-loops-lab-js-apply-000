edb6711cdaf70e07e227438c862884663d112f67
Dennis Gunden <dennisgunden@gmail.com>
2019-12-10 17:47:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // add solution here
+function theBeatlesPlay(arrOfMus, arrOfInst) {
+  var newArray = [];
+  for(let i = 0; i < arrOfMus.length; i++) {
+    return 'John Lennon played the guitar';
+  }
+}
